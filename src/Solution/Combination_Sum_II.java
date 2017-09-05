1fa56985221a170bed07a7608663ce6b2c4c3a77
zhangwei <zw_pku@163.com>
2017-09-05 23:06:31 +0800

    combination

--- src/Solution/Combination_Sum_II.java
+++ src/Solution/Combination_Sum_II.java
@@ -6,12 +6,25 @@ import java.util.List;
 
 /**
  * Created by zhangwei on 2017/8/13.
+ *
+ * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+ Each number in C may only be used once in the combination.
+
+ Note:
+ All numbers (including target) will be positive integers.
+ The solution set must not contain duplicate combinations.
+ For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8,
+ A solution set is:
+ [
+ [1, 7],
+ [1, 2, 5],
+ [2, 6],
+ [1, 1, 6]
+ ]
  */
 public class Combination_Sum_II {
-    public static void main(String[] args) {
-
-    }
-    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> tmp = new ArrayList<>();
